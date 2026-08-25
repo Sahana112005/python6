e660110fd3c31a7215780bcf5c19b3738a695c73
Marimganti Lakshmi Sahana <285583609+Sahana112005@users.noreply.github.com>
2026-08-25 18:21:40 +0530

    Add files via upload

---
+++ 17.py
@@ -0,0 +1,71 @@
+"""a=int(input())               #23
+n=a%10                          #3
+b=a//10                         #2
+result=n*1000+b                 #3002
+print(result)
+"""  
+"""num=int(input())              #23
+if num>0:                          #T
+    print("positive")                
+else:
+    print("Negative")
+"""
+"""num=int(input())              #35
+if num%5:                           #T
+    print("divisible by 5")
+else:
+    print("not divisible by 5")
+""" 
+"""temp=int(input())                #45
+if temp>50:
+    print("its hot")
+else:
+    print("its  not hot")                 #its not hot
+"""    
+"""salary=int(input())                     #600000
+if salary>500000:                          #T
+    bonus=salary*10/100                        #60000
+bonus=salary+bonus                         #660000  
+print("total salary:",salary)              
+"""
+"""number_of_tickets=int(input()) #5 (if)          #7 (else)
+if number_of_inputs<=6:
+    print("booking allowed")
+else:
+    print("booking limit exceeded")
+"""
+"""day=input()
+
+if day=="saturday" or day=="sunday":
+    print("discount applicable")
+else:
+    print("no discount")
+"""
+"""day=input()
+
+if day=="saturday":
+    print("discount applicable on saturday")
+elif day=="sunday":
+    print("discount applicable on sunday")
+else:
+    print("no discount")
+"""
+"""age=int(input())
+if age<5:
+    print("free")
+elif age>=5 and age<=12:
+    print("its a child ticket")
+elif age>= 13 and age<=59:
+    print("its a regular ticket")
+else:
+    print("its a senior citizen")
+
+""" 
+"""num=int(input())          
+if num>0:
+    print("positive")
+elif num<0:
+    print("negative")
+else:
+    print("zero")
+"""
